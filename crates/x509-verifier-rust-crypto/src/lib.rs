99ea5ea856e10192bb67a8e0a6fcbe26579a4961
Preston Ong <preston4896@me.com>
2025-10-27 18:28:09 +0800

    added pico precompiles and feature gates

--- crates/x509-verifier-rust-crypto/src/lib.rs
+++ crates/x509-verifier-rust-crypto/src/lib.rs
@@ -1,10 +1,12 @@
 // Override the crates by the precompiles
 
+#[cfg(feature = "pico")]
+pub extern crate sha2_pico as sha2;
 #[cfg(feature = "sp1")]
 pub extern crate sha2_sp1 as sha2;
-#[cfg(all(feature = "risc0", not(feature = "sp1")))]
+#[cfg(all(feature = "risc0", not(feature = "sp1"), not(feature = "pico")))]
 pub extern crate sha2_risc0 as sha2;
-#[cfg(all(not(feature = "sp1"), not(feature = "risc0")))]
+#[cfg(all(not(feature = "sp1"), not(feature = "risc0"), not(feature = "pico")))]
 pub use sha2;
 
 #[cfg(feature = "sp1")]
