41195af6e5fcea6d27464e3c2b14f4dd3fab40d1
Preston Ong <preston4896@me.com>
2025-11-26 12:13:06 +0800

    USE_DOCKER env for deterministic builds

--- crates/risc0-methods/build.rs
+++ crates/risc0-methods/build.rs
@@ -1,3 +1,23 @@
+use risc0_build::{embed_methods_with_options, DockerOptionsBuilder, GuestOptionsBuilder};
+use std::collections::HashMap;
+use std::path::PathBuf;
+
 fn main() {
-    risc0_build::embed_methods();
+    let manifest_dir = PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let mut builder = GuestOptionsBuilder::default();
+    if std::env::var("USE_DOCKER").is_ok() {
+        let docker_options = DockerOptionsBuilder::default()
+            .root_dir(manifest_dir.join("../../"))
+            .build()
+            .unwrap();
+
+        builder.use_docker(docker_options);
+    }
+    let guest_options = builder.build().unwrap();
+
+    // Build both verifier and aggregator with the same options
+    embed_methods_with_options(HashMap::from([
+        ("risc0-verifier", guest_options.clone()),
+        ("risc0-aggregator", guest_options),
+    ]));
 }
--- crates/sp1-methods/build.rs
+++ crates/sp1-methods/build.rs
@@ -1,4 +1,14 @@
+use sp1_build::{build_program_with_args, BuildArgs};
+
 fn main() {
-    sp1_build::build_program_with_args("../sp1-methods/sp1-verifier", Default::default());
-    sp1_build::build_program_with_args("../sp1-methods/sp1-aggregator", Default::default());
+    build_program_with_args("../sp1-methods/sp1-verifier", get_build_args());
+    build_program_with_args("../sp1-methods/sp1-aggregator", get_build_args());
+}
+
+fn get_build_args() -> BuildArgs {
+    let use_docker = std::env::var("USE_DOCKER").is_ok();
+    BuildArgs {
+        docker: use_docker,
+        ..Default::default()
+    }
 }
